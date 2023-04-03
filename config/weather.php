5b8398f4669598346735b2b7c095f4ab611b103c
Richard Skinner <richard.skinner@gunpowderdigital.com>
2023-04-03 17:54:50 +0100

    Security fix. Removed API Key from config.

--- config/weather.php
+++ config/weather.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'key' => 'c7dfe0dff1e28be415a120712aeb338a',
+    'key' => env('OPEN_WEATHER_MAPS_API_KEY'),
     'source' => [
         'forecast' => 'api.openweathermap.org/data/2.5/forecast?appid=%s&q=%s',
         'geo' => 'api.openweathermap.org/geo/1.0/direct?appid=%s&q=%s',
